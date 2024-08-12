ed39c36acf71b8f463736e3b51dd8a3a36880d3e
BinhTran <fam.out16@gmail.com>
2024-08-12 21:51:03 +0700

    Add cac files cua bai tap hien thi mot so luong nhap truoc cac so nguyen to dau tien

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace SUDUNGCAULENHLAP
+{
+    class HienThi20SoNguyenToDauTien
+    {
+        static void Main(string[] args)
+        {
+            int numbers = int.Parse(Console.ReadLine());
+            int count = 0;
+            int n = 2;
+            while (count < numbers)
+            {
+                int divisorcount = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    if (n % i == 0)
+                    {
+                        divisorcount++;
+                    }
+                }
+                if (divisorcount == 2)
+                {
+                    Console.WriteLine(n);
+                    count++;
+                }
+                n++;
+            }
+
+        }
+    }
+}
